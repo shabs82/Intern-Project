6774b511b4b4c91ab5ff30fce1710e224f89ae54
shabs <43846967+shabs82@users.noreply.github.com>
2021-08-27 11:10:04 +0200

    navbar

--- saffron-cosmetics/src/app/app.module.ts
+++ saffron-cosmetics/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { TopnavbarComponent } from './shared/topnavbar/topnavbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    TopnavbarComponent
   ],
   imports: [
     BrowserModule,
