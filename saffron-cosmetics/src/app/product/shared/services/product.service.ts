369c0be9751080c7d867953435f905284ff5d676
shabs <43846967+shabs82@users.noreply.github.com>
2021-10-30 14:13:51 +0200

    product service get

---
+++ saffron-cosmetics/src/app/product/shared/services/product.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Product} from "../model/product";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+
+ readAllProducts(): Observable<Product[]>{
+  return this.http.get<Product[]>('https://localhost:44375/api/product');
+ }
+
+  getProductById(id: string): Observable<Product>{
+    return this.http.get<Product>('https://localhost:44375/api/product/id');
+  }
+}
