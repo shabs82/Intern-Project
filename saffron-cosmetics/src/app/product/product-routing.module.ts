3d06edd8af608c1e42cf21b47c7dd2fd9d8ab7c6
shabs <43846967+shabs82@users.noreply.github.com>
2021-11-07 13:52:02 +0100

    image added

--- saffron-cosmetics/src/app/product/product-routing.module.ts
+++ saffron-cosmetics/src/app/product/product-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product.component';
 import {ListComponent} from "./list/list.component";
+import {DetailComponent} from "./detail/detail.component";
 
 const routes: Routes = [{ path: '', component: ProductComponent },
-                        { path: 'product-list/:secondaryClassId', component: ListComponent }];
+                        { path: 'product-list/:secondaryClassId', component: ListComponent },
+                        {path: 'product-detail/: id', component: DetailComponent}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
