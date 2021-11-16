1def2db9beab44f232aa0fdc45b8451b9766f3dd
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2021-11-16 19:41:49 +0100

    Removed comments and rearrangement

--- saffron-cosmetics/src/app/user/shared/services/authentication-service.ts
+++ saffron-cosmetics/src/app/user/shared/services/authentication-service.ts
@@ -44,27 +44,7 @@ export class AuthenticationService {
     }
     return this.isLoggedAdmin;
   }
-  //Add Api Urls
-  // login(username: string, password: string): Observable<boolean> {
-  //   return this.http.post<any>(environment.apiURL + 'token', {username, password})
-  //     .pipe(map(response => {
-  //       const token = response.token;
-  //       //login successful if there's a jwt token in the response.
-  //       const decodedToken = jwt_decode(token);
-  //       if (token){
-  //         //Store username and jwt token in local storage to keep user logged in between page refreshes
-  //         this.setUpStorage({decodedToken: decodedToken, response: response});
-  //         this.isLoggedInSubject.next(true);
-  //         this.isLoggedAdmin.next(true);
-  //         window.location.reload();
-  //         return true;
-  //       } else{
-  //         this.isLoggedInSubject.next(false);
-  //         this.isLoggedAdmin.next(false);
-  //         return false;
-  //       }
-  //     }));
-  // }
+
 
   login(username: string, password: string): Observable<boolean>{
     return this.http.post<any>(environment.apiURL + '/api/token', {username, password})
@@ -86,38 +66,27 @@ export class AuthenticationService {
     httpOptions.headers = httpOptions.headers.set('Authorization','Bearer' + this.getToken());
     return this.http.post<User>(environment.apiURL + '/api/user', user);
 
-    // return this.http.post<any>(environment.apiURL + '/api/user', {username, password})
-    //   .pipe(map(response =>{
-    //     const token = response && response.token;
-    //     console.log(response);
-    //     if(token){
-    //       localStorage.setItem('currentUser', JSON.stringify({username: username, token: token }));
-    //       return true;
-    //     } else {
-    //       return false;
-    //     }
-    //   }));
   };
 
-  setUpStorage({decodedToken, response}: { decodedToken: any, response: any }): void {
-    let currentUser = null;
-    if ( 'Administrator' === decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']){
-      currentUser = JSON.stringify({
-        username: decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'],
-        IsAdmin: true,
-        token: response.token
-      });
-    } else {
-      currentUser = JSON.stringify({
-        username: decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'],
-        currentRole: decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'],
-        token: response.token
-      });
-    }
-    this.logout(); // Delete existing user
-    console.log(currentUser);
-    localStorage.setItem('currentUser', currentUser);
-  }
+  // setUpStorage({decodedToken, response}: { decodedToken: any, response: any }): void {
+  //   let currentUser = null;
+  //   if ( 'Administrator' === decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']){
+  //     currentUser = JSON.stringify({
+  //       username: decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'],
+  //       IsAdmin: true,
+  //       token: response.token
+  //     });
+  //   } else {
+  //     currentUser = JSON.stringify({
+  //       username: decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'],
+  //       currentRole: decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'],
+  //       token: response.token
+  //     });
+  //   }
+  //   this.logout(); // Delete existing user
+  //   console.log(currentUser);
+  //   localStorage.setItem('currentUser', currentUser);
+  // }
 
 
   getToken(): string {
