e7ef2c1b4ebd0a11597d39c72d1836e05742d3c0
dpankov91 <dpankov91@gmail.com>
2021-11-21 20:39:42 +0200

    modified navbar

--- saffron-cosmetics/src/app/shared/navbar/navbar.component.ts
+++ saffron-cosmetics/src/app/shared/navbar/navbar.component.ts
@@ -11,8 +11,20 @@ import {Router} from "@angular/router";
 })
 export class NavbarComponent implements OnInit {
 
-  eyeArray = [{id:8,label:'Eye Shadow'}, {id:9,label:'Eye Liner'}, {id:10,label:'Eye Lenses'}, {id:11,label:'Eyebrows'}, {id:12,label:'Mascara'}, {id:14,label:'Eye Pencil'},
-    {id:15,label:'Eye Lashes'}, {id:23,label:'Kajal & Eye Shade'}]
+  eyeArray = [{id:6,label:'Eye Shadow'}, {id:5,label:'Eye Liner'}, {id:4,label:'Eye Lenses'}, {id:3,label:'Eyebrows'}, {id:2,label:'Mascara'}, {id:1,label:'Eye Pencil'},
+    {id:8,label:'Eye Lashes'}, {id:7,label:'Kajal & Eye Shade'}];
+
+  lipsArray = [{id:10,label:'Lip Stick'}, {id:11,label:'Lip Gloss'}, {id:12,label:'Lip Liner'}, {id:13,label:'Lip Pencil'}];
+
+  nailArray = [{id:14,label:'Nail Polish'}, {id:15,label:'Nail Accessories'}];
+
+  faceArray = [{id:16,label:'Compact Powder'}, {id:17,label:'Foundation'}, {id:18,label:'Blushes'}, {id:19,label:'Concealer'}, {id:20,label:'Primer'}, {id:21,label:'Highlighters / Bronzing'} ];
+
+  accessoriesArray = [{id:22,label:'Applicators'}, {id:23,label:'Brushes'}, {id:24,label:'Vanity Case'}];
+
+  fragrancesArray = [{id:25,label:'Mens Perfume'}, {id:26,label:'Womens Fragrance'}, {id:27,label:'Unisex Fragrance'}];
+
+  gifsetsArray = [{id:28,label:'Gift Sets'}];
 
   constructor(private productService: ProductService, private router: Router) {
   }
