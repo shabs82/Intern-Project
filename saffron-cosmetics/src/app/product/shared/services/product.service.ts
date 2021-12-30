589fa1f5f956fbf0e5124911503f2e9128eb731c
dpankov91 <dpankov91@gmail.com>
2021-12-30 19:30:46 +0200

    deployment competed

--- saffron-cosmetics/src/app/product/shared/services/product.service.ts
+++ saffron-cosmetics/src/app/product/shared/services/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Product} from "../model/product";
 import {Observable} from "rxjs";
+import {environment} from "../../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -11,15 +12,15 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
  readAllProducts(): Observable<Product[]>{
-  return this.http.get<Product[]>('https://localhost:44375/api/product');
+  return this.http.get<Product[]>(environment.apiURL + '/api/product');
  }
 
   getProductById(id: string): Observable<Product>{
-    return this.http.get<Product>('https://localhost:44375/api/product/'+ id);
+    return this.http.get<Product>(environment.apiURL + '/api/product/'+ id);
   }
 
   async getProductsBySecondaryClassId(secondaryClassId: number): Promise<any[]> {
-    const uspromise = await this.http.get<any>('https://localhost:44375/api/secondarycategory/' + secondaryClassId).toPromise();
+    const uspromise = await this.http.get<any>(environment.apiURL + '/api/secondarycategory/' + secondaryClassId).toPromise();
     return uspromise.map((a: { id: any; name: any; productCode: any; price: any; availability: any; description: any; }) => {
       console.log(a);
       const id = a.id;
