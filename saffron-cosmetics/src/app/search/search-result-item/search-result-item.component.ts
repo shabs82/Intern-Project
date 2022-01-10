16de944118feb20f3e98abe2f5c7f3f8459b1604
shabs <43846967+shabs82@users.noreply.github.com>
2022-01-10 19:34:05 +0100

    some changes

--- saffron-cosmetics/src/app/search/search-result-item/search-result-item.component.ts
+++ saffron-cosmetics/src/app/search/search-result-item/search-result-item.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from '../search.service';
 import {WishlistService} from "../../wishlist/shared/wishlist.service";
+import {ProductService} from "../../product/shared/services/product.service";
+import {SearchOption} from "../searchOption";
+import {ShoppingCartService} from "../../shopping-cart/shared/shopping-cart.service";
 
 @Component({
   selector: 'app-search-result-item',
@@ -8,7 +11,37 @@ import {WishlistService} from "../../wishlist/shared/wishlist.service";
   styleUrls: ['./search-result-item.component.scss'],
 })
 export class SearchResultItemComponent implements OnInit {
-  constructor(public searchService: SearchService) {}
+  addedToWishlist: boolean = false;
 
-  ngOnInit(): void {}
+  constructor(public searchService: SearchService, private prodService: ProductService,
+              private wishlistService: WishlistService,private cartService: ShoppingCartService) {
+  }
+
+  ngOnInit(): void {
+
+  }
+  refreshPage() {
+    window.location.reload();
+  }
+
+  addToFavourites(selectedOption: any) {
+    this.wishlistService.addToWishList(selectedOption);
+    this.addedToWishlist = false;
+    this.refreshPage();
+
+  }
+
+  removeFromFavourites(selectedOption: any) {
+    this.wishlistService.removeFromWishlist(selectedOption);
+    this.addedToWishlist = false;
+    this.refreshPage();
+
+  }
+
+  async addToCart(product: SearchOption) {
+    if(product.id != null){
+      await this.prodService.getProductById(product.id).subscribe(p => this.cartService.addToCart(p))
+    }
+
+  }
 }
